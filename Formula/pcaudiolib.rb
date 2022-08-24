31a8ddb33840a10c83fbeeb9defd54ee76613b63
robert jakub <rj@project2.pl>
2022-08-24 20:18:14 +0200

    updates

--- Formula/pcaudiolib.rb
+++ Formula/pcaudiolib.rb
@@ -1,7 +1,9 @@
 class Pcaudiolib < Formula
   desc "Portable C Audio Library"
   homepage ""
-  url "https://github.com/espeak-ng/pcaudiolib/releases/download/1.2/pcaudiolib-1.2.tar.gz"
+  version "1.2"
+  url "https://github.com/espeak-ng/pcaudiolib/releases/download/#{version}/pcaudiolib-#{version}.tar.gz",
+      verified: "github.com/espeak-ng/pcaudiolib/"
   sha256 "6fae11e87425482acbb12c4e001282d329be097074573060f893349255d3664b"
   license "GPL-3.0-only"
 
